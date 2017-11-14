0af238b628c54f83a81f33a3a081732f11776a4c
chickencat <31521769+chickencat@users.noreply.github.com>
2017-11-13 17:48:53 -0800

    Finalizing the project

--- designs.js
+++ designs.js
@@ -1,52 +1,61 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-
-const height = $("#input_height").val();
-const width = $("#input_width").val();
-let table = $("#pixel_canvas");
-let table_row = $('tr');
-let table_data = $('td');
-
-
-
+const table = $("#pixel_canvas");
+const height_input = $("#input_height");
+const width_input = $("#input_width");
+// Function to draw the grid
 function makeGrid() {
 	// Clear the table first
 	table.empty();
-	// Draws the grid
-	for (let row = 0; row < width; row++) {
-		table.append("<tr></tr>");
-		for (let col = 0; col < height; col++) {
-			table_row.last().append('<td></td>');
-		}
-	};
 
-	// Add the selected color when clicked
-	function changeColor() {
-		table_data.click(function() {
-			let color = $("#colorPicker").val();
-			$(this).css('background-color', color);
-		});
+	let height = height_input.val();
+	let width = width_input.val();
+	// Draws the grid
+	for (let row = 0; row < height; row++) {
+		table.append("<tr></tr>");
+		for (let col = 0; col < width; col++) {
+			$('tr').last().append('<td></td>');
+		}
 	};
 };	
 
-// call makeGrid() when the player clicks Submit
+// Changes the grid color when it's clicked
+let isDown = false; // Boolean for the mouseover function
+
+$('body').mousedown(function() {
+	isDown = true;
+});
+
+$('body').mouseup(function() {
+	isDown = false;
+});
+
+function changeColor() {
+	// Colors the grid with mouseover
+	$('td').mouseover(function() {
+		if (isDown) {
+		let color = $("#colorPicker").val();
+		$(this).css('background-color', color);
+		}
+	});
+	// Colors the grid when clicked
+	$('td').click(function() {
+	let color = $("#colorPicker").val();
+	$(this).css('background-color', color);
+	});
+};
+
+// Creates the grid when the submit button is
 $("#submit").click(function(event) {
-	makeGrid();
-	console.log("Submit clicked");
-	console.log("H = " + height + " W = " + width);
-	// Stops page from refreshing
 	event.preventDefault();
+	makeGrid();
+	changeColor();
 });
 
 // When the clear grid botton is clicked
 $("#clear_grid").click(function() {
 	table.empty();
-	console.log("Success!");
 	// Change color input back to black
-	$("#colorPicker").val("#000000")
+	$("#colorPicker").val("#000000");
+	// Change the height and width value back to 1
+	$("#input_height").val(1);
+	$("#input_width").val(1);
 });
-
-
